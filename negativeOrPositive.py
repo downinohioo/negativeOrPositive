5fc8f4513e5a430cff3b567f0baac120e18d00df
downinohioo <129334412+downinohioo@users.noreply.github.com>
2023-05-10 19:02:14 -0400

    Add files via upload

---
+++ negativeOrPositive.py
@@ -0,0 +1,7 @@
+negativeOrPositive = int(input("Enter a number: "))
+if negativeOrPositive < 0:
+    print("Your number is a negative number")
+elif negativeOrPositive > 0:
+    print("Your number is a positive number")
+else:
+    print("Your number is zero")
